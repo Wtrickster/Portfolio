4852eb616a0377fc62d21f4f0ad2067ada95051c
Joseph <JSPenland97@gmai.com>
2022-01-07 21:25:57 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as HashRouter, Route, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
