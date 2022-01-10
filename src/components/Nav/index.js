7d32014753b9bc641aee9513accf2810bdec04d6
Joseph <JSPenland97@gmai.com>
2022-01-09 23:17:54 -0500

    navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components"
 import Contact from "../Dropdown";
+import Container from "../Grid"
 
 const NavUnlisted = styled.ul`
 
@@ -27,6 +28,7 @@ const NavUnlisted = styled.ul`
 
 function Nav() {
   return (
+    <Container>
     <NavUnlisted>
         <NavLink to="/About" className={window.location.pathname === "/About" ? "nav-item nav-link active" : "nav-item nav-link"}activeClassName="current" exact>
           <li className="underline">Home</li>
@@ -34,10 +36,9 @@ function Nav() {
         <NavLink to="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-item nav-link active" : "nav-item nav-link"}activeClassName="current" exact>
           <li className="underline">Projects</li>
         </NavLink>
-        <Nav>
-          <Contact />
-        </Nav>
     </NavUnlisted>
+    <Contact />
+    </Container>
   );
 }
 export default Nav;
